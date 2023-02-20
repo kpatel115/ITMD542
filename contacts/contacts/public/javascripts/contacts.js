0c306fe3bdd92d40c11d25db7a0475651ea9b79f
kpatel115 <kp9338448@gmail.com>
2023-02-19 21:40:42 -0600

    created two html pages - homepage and view page

---
+++ contacts/contacts/public/javascripts/contacts.js
@@ -0,0 +1,9 @@
+allContacts = []
+contactExample = {
+  contactID: 001,
+  firstName: "contact",
+  lastName: "example",
+  email: "example@gmail.com",
+  notes: "",
+  dateTime: 858
+}
--- contacts/contacts/routes/index.js
+++ contacts/contacts/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express Contact App' });
+  res.render('index', { title: "Karan's Express Contact App" });
 });
 
 module.exports = router;
