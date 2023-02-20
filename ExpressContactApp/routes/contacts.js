db21add383af9c09941657a5a88ec02a05d1ad76
kpatel115 <kp9338448@gmail.com>
2023-02-20 17:29:25 -0600

    setup html templates with buttons and urls and links work

--- ExpressContactApp/routes/contacts.js
+++ ExpressContactApp/routes/contacts.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-const { randomUUID } = require("node:crypto");
-
+//const { randomUUID } = require("node:crypto");
+const contactsRepo = require('../src/contactsMemoryRepository');
 let data = [
   {name: "karan", id: "ddad031e-e030-419a-9518-2f16534edeaf"},
   {name: "patel", id: "e4a9c8f6-4bb8-44f5-8915-8df78500f6e3"}
@@ -10,6 +10,7 @@ let data = [
 
 /* GET Contacts Database. */
 router.get('/', function(req, res, next) {
+  const data = contactsRepo.findAll()
   res.render('contacts', { title: 'Express Contacts', contacts: data});
 });
 
@@ -25,12 +26,22 @@ router.post('/add', function(req, res, next) {
     res.render('contacts_add', { title: "Add a Contact", msg: "Please fill out the form"});
   } else {
     // add contact to database
+    contactsRepo.create({name: req.body.firstName.trim()})
     res.redirect('/contacts');
     res.send('contact created');
   }
   
 });
 
-
+/* GET Single Contact */ 
+router.get('/:uuid', function(req, res, next) {
+  const contact = contactsRepo.findById(req.params.uuid);
+  if (contact) {
+    res.render('contact', { title: 'Your Contact', contact: contact });
+  } else {
+    res.redirect('/contacts');
+  }
+  
+});
 
 module.exports = router;
--- ExpressContactApp/src/contactsMemoryRepository.js
+++ ExpressContactApp/src/contactsMemoryRepository.js
@@ -1,3 +1,25 @@
 // Memory Repository
 // handling all data access
+const crypto = require('crypto');
+const db = new Map();
+
+  
+db.set('ddad031e-e030-419a-9518-2f16534edeaf', {name: "karan", id: "ddad031e-e030-419a-9518-2f16534edeaf"})
+db.set('e4a9c8f6-4bb8-44f5-8915-8df78500f6e3', {name: "patel", id: "e4a9c8f6-4bb8-44f5-8915-8df78500f6e3"})
+//db map with two objects
+
+// repo set to object for application
+const repo = {
+  findAll: () => Array.from(db.values()),
+  findById: (uuid) => db.get(uuid),
+  create: (contact) => {
+    const newContact = {
+      id: crypto.randomUUID(),
+      name: contact.name,
+    };
+    db.set(newContact.id, newContact)
+  },
+};
+
+module.exports = repo;
 
